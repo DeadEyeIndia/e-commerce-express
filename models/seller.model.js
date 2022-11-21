a1bdae1d15e3c92f47f311c25d731e69f2963e2e
DeadEyeIndia <rahulbattin24@gmail.com>
2022-11-21 18:25:41 +0530

    Modified seller controller and model

--- models/seller.model.js
+++ models/seller.model.js
@@ -3,7 +3,7 @@ import validator from "validator";
 
 const SellerSchema = new mongoose.Schema(
   {
-    name: {
+    businessname: {
       type: String,
       required: [true, "Enter required fields"],
       minlength: [5, "Name is too short"],
