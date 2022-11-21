29b3f63eca1fd7f4c2a70f16ec0967068125fc71
DeadEyeIndia <rahulbattin24@gmail.com>
2022-11-21 18:11:17 +0530

    Seller Model modified

--- models/seller.model.js
+++ models/seller.model.js
@@ -36,6 +36,17 @@ const SellerSchema = new mongoose.Schema(
       },
       unique: [true, "Number already taken"],
     },
+    category: {
+      type: String,
+      required: true,
+      enum: [
+        "Clothing",
+        "Computer & accessories",
+        "Electronics",
+        "Footwear",
+        "Watches",
+      ],
+    },
     description: {
       type: String,
       required: [true, "Description is required"],
